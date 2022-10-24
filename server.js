a82e76d6e69c83bba1a84a2c957b9a95beca40a3
hashamuddin1 <73032957+hashamuddin1@users.noreply.github.com>
2022-10-24 21:21:19 +0500

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 require('dotenv').config();
-const port = process.env.port;
+const port = process.env.PORT;
 const router = require('./router/route')
 require('./db/prodb');
 app.use(express.json());
